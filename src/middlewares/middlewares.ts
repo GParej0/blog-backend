a2169d0e247469de5a1c6a0fe852fbb8d565e51b
GParej0 <195317371+GParej0@users.noreply.github.com>
2026-08-15 07:53:28 +0200

    feat: add auth middleware to verify jwt tokens

---
+++ src/middlewares/middlewares.ts
@@ -0,0 +1,29 @@
+import {Request, Response, NextFunction} from "express"
+import  jwt  from "jsonwebtoken";
+function verifyToken(req:Request, res:Response, next:NextFunction) {
+
+  const bearerHeader = req.headers['authorization'];
+
+if (!bearerHeader || !bearerHeader.startsWith("Bearer ")) {
+
+  return res.status(401).json({ error: "Token no proporcionado o formato inválido" });
+} 
+
+const token = bearerHeader.split(" ")[1]
+
+try{
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+  (req as any).user = decoded;
+
+  next()
+
+} catch(error){
+
+  return res.status(403).json({ error: "Invalid or expired token" });
+
+}
+
+}
+
+export default verifyToken
